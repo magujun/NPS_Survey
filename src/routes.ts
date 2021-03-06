cf5b39e764bf7a0771e526f911949ae728afaafb
magujun <75567460+magujun@users.noreply.github.com>
2021-03-06 15:49:55 +0100

    chore: first major update - added frontend

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
-import { Router } from 'express';
+import express, { Request, Response, NextFunction, Router } from 'express';
 import { UserController } from './controllers/UserController';
 import { SurveyController } from './controllers/SurveyController';
 import { SendMailController } from './controllers/SendMailController';
 import { AnswerController } from './controllers/AnswerController';
 import { NpsController } from './controllers/NpsController';
+import cors from 'cors';
 
 const router = Router();
 
@@ -13,10 +14,28 @@ const sendMailController = new SendMailController();
 const answerController = new AnswerController();
 const npsController = new NpsController();
 
+const app = express();
+
+app.use(cors());
+
+// Enable CORS
+app.use((request: Request, response: Response, _next: NextFunction) => {
+	response.header('Access-Control-Allow-Origin', 'localhost'); // match the domain you will make the request from
+	response.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	_next();
+});
+
+// Enable pre-flight across-the-board
+app.options('*', cors()); // include before other routes
+
 router.post('/users', userController.create);
 router.post('/surveys', surveyController.create);
 router.get('/surveys', surveyController.show);
 router.post('/sendMail', sendMailController.execute);
 router.get('/answers/:value', answerController.execute);
 router.get('/nps/:survey_id', npsController.execute);
+
 export { router };
