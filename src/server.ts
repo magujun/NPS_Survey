00abad0de82b1c575b779f8d4806ecb93ae7fc23
magujun <75567460+magujun@users.noreply.github.com>
2021-03-06 16:17:31 +0100

    chore: first major update - added frontend

--- src/server.ts
+++ src/server.ts
@@ -9,10 +9,27 @@
 // app.use(express.json());
 // app.use(router);
 
+import cors from 'cors';
+import { NextFunction, Request, Response } from 'express';
 import { app } from './app';
 
 const PORT = 8000;
   
+app.use(cors());
+
+// Enable CORS
+app.use((request: Request, response: Response, _next: NextFunction) => {
+	response.header('Access-Control-Allow-Origin', 'localhost'); // match the domain you will make the request from
+	response.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	_next();
+});
+
+// Enable pre-flight across-the-board
+app.options('*', cors()); // include before other routes
+
 app.listen(PORT, () => console.log('⚡️ Node server is running at port:',PORT,'⚡️'));
 
 // /**
@@ -39,24 +56,4 @@ app.listen(PORT, () => console.log('⚡️ Node server is running at port:',PORT
 // app.post("/", (request, response)=> {
 //     // Recebeu os dados para salvar
 //     return response.json({message: "Os dados foram salvos com sucesso!"});
-// });
-
-// app.use(cors());
-
-// // Enable CORS
-// app.use((request: Request, response: Response, _next: NextFunction) => {
-// 	response.header('Access-Control-Allow-Origin', 'localhost'); // match the domain you will make the request from
-// 	response.header(
-// 		'Access-Control-Allow-Headers',
-// 		'Origin, X-Requested-With, Content-Type, Accept'
-// 	);
-// 	_next();
-// });
-
-// // Enable pre-flight across-the-board
-// app.options('*', cors()); // include before other routes
-
-// app.post('*', cors(), function (request: Request, response: Response, _next: NextFunction) {
-// 	response.json({ msg: 'This is CORS-enabled for all origins!' });
-// 	_next();
 // });
